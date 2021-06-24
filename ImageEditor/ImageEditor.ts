a4229acd53dc05e9e3022c3b7b31f7a415e233e5
scd7896 <scd1212@naver.com>
2021-06-24 21:29:51 +0900

    Fix: 개발 환경에서 테스트 할 수 있게함

--- ImageEditor/ImageEditor.ts
+++ ImageEditor/ImageEditor.ts
@@ -1,9 +1,54 @@
+import { Point } from "fabric/fabric-impl";
 import Canvas from "./Canvas";
 import Options from "./Options";
 import Sticker from "./Sticker";
 import ToolButtons from "./ToolButtons";
 import { getResizeFillWidthHeight } from "./util/Resize";
+declare var process: {
+	env: {
+		NODE_ENV: string;
+	};
+};
 
+class Pointer {
+	x: number | null;
+	y: number | null;
+
+	constructor() {
+		this.x = null;
+		this.y = null;
+	}
+
+	pointInitSet() {
+		this.x = null;
+		this.y = null;
+	}
+
+	setPoint({ x, y }: { x: number; y: number }) {
+		this.x = x;
+		this.y = y;
+	}
+}
+class DevelopDiffList {
+	public flag: boolean;
+	public count: number;
+
+	constructor() {
+		this.flag = false;
+		this.count = 2;
+		window.addEventListener("keydown", (ev) => {
+			if (ev.key === "z") {
+				this.flag = true;
+			} else {
+				this.flag = false;
+			}
+		});
+
+		window.addEventListener("keyup", () => {
+			this.flag = false;
+		});
+	}
+}
 class ImageEditor {
 	private canvas: Canvas;
 	private option: any;
@@ -11,10 +56,17 @@ class ImageEditor {
 	private wrapper: HTMLDivElement;
 	private canvasWrapper: HTMLDivElement;
 	private imgUrl: string;
+	private develoVersionKey: DevelopDiffList | null;
+	private pointer1: Pointer;
+	private pointer2: Pointer;
 
 	constructor(wrapper: string | HTMLDivElement, options) {
 		let target: HTMLDivElement;
+		this.pointer1 = new Pointer();
+		this.pointer2 = new Pointer();
 		this.option = options;
+		this.develoVersionKey = new DevelopDiffList();
+
 		if (typeof wrapper === "string") {
 			target = document.querySelector(`#${wrapper}`) as HTMLDivElement;
 		} else {
@@ -42,23 +94,52 @@ class ImageEditor {
 		this.canvasWrapper.ontouchstart = this.start_handler.bind(this);
 		this.canvasWrapper.ontouchmove = this.move_handler.bind(this);
 
-		// this.canvasWrapper.ontouchcancel = end_handler;
-		// this.canvasWrapper.ontouchend = end_handler;
+		this.canvasWrapper.ontouchcancel = this.end_handler.bind(this);
+		this.canvasWrapper.ontouchend = this.end_handler.bind(this);
+	}
+
+	end_handler() {
+		this.pointer1.pointInitSet();
+		this.pointer2.pointInitSet();
 	}
 
 	move_handler(ev: TouchEvent) {
-		ev.stopImmediatePropagation();
 		ev.preventDefault();
-		// console.log(ev);
-		if (ev.touches.length >= 2) {
+
+		if (ev.touches.length >= this.develoVersionKey.count || this.develoVersionKey.flag) {
+			ev.stopImmediatePropagation();
+			const touch1 = ev.touches.item(0);
+			const touch2 = ev.touches.item(1);
+			const { x: firstX, y: firstY } = this.pointer1;
+			const { x: secondX, y: seconddY } = this.pointer2;
+			let firstDiffX = touch1.clientX - firstX;
+			let firstDiffY = touch1.clientY - firstY;
+
+			if (touch2) {
+				const secondDiffX = touch2.clientX - secondX;
+				const secondDiffY = touch2.clientY - seconddY;
+				firstDiffX += secondDiffX;
+				firstDiffY += secondDiffY;
+				this.pointer2.setPoint({ x: touch2.clientX, y: touch2.clientY });
+			}
+
+			this.canvasWrapper.scroll(this.canvasWrapper.scrollLeft - firstDiffX, this.canvasWrapper.scrollTop - firstDiffY);
+			this.pointer1.setPoint({ x: touch1.clientX, y: touch1.clientY });
+
 			return false;
 		}
 	}
 
-	start_handler(ev) {
+	start_handler(ev: TouchEvent) {
 		ev.preventDefault();
-		console.log(ev);
-		if (ev.targetTouches.length == 2) {
+		if (ev.targetTouches.length >= this.develoVersionKey.count || this.develoVersionKey.flag) {
+			const touch1 = ev.touches.item(0);
+			const touch2 = ev.touches.item(1);
+			this.pointer1.setPoint({ x: touch1.clientX, y: touch1.clientY });
+
+			if (touch2) {
+				this.pointer2.setPoint({ x: touch2.clientX, y: touch2.clientY });
+			}
 			return false;
 		}
 	}
@@ -87,7 +168,6 @@ class ImageEditor {
 		const resizeImg = new Image();
 		resizeImg.width = width;
 		resizeImg.height = height;
-		console.log(width, height);
 		resizeImg.src = this.imgUrl;
 		resizeImg.onload = () => {
 			const canvas = document.createElement("canvas");
@@ -97,7 +177,6 @@ class ImageEditor {
 			canvas.width = width;
 			canvas.height = height;
 
-			// this.canvasWrapper.appendChild(dummyDiv);
 			this.canvasWrapper.appendChild(canvas);
 			this.canvas = new Canvas(canvas, {
 				left: this.canvasWrapper.clientWidth / 4,
