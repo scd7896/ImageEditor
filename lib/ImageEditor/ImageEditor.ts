9e241b0a5a6f7eda74488a9251150fc0b50179d9
scd7896 <scd1212@naver.com>
2021-08-16 17:47:41 +0900

    export도 시켜봄봄

--- lib/ImageEditor/ImageEditor.ts
+++ lib/ImageEditor/ImageEditor.ts
@@ -16,7 +16,7 @@ import PenEvent from "./events/PenEvent";
 
 const footerButtons = ["shape", "pen", "sticker"];
 const headerButtons = ["close", "undo", "redo", "remove", "download"];
-class ImageEditor {
+export class ImageEditor {
 	private canvas: Canvas;
 	private option: any;
 	private stickerWrapper: HTMLDivElement;
